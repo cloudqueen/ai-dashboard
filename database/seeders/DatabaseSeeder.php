c4bbdf1f05c5ecfef2c1bfff4dcff6a2b1ca9313
Stefanie Brockelt <info@cloudqueen.de>
2026-04-13 16:02:12 +0200

    Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Steffi',
+            'email' => 'info@cloudqueen.de',
+            'password' => bcrypt('secret'),
         ]);
     }
 }
